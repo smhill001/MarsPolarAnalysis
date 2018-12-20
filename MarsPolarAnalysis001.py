c24096335a1d1c0e66b33129e15a723981d00aba
Steven Hill <smhill001@gmail.com>
2018-12-20 06:28:48 -0700

    Refacatoring 1
    Modified Mars Polar Library to use the base class readtextfilelines from the Configuration Files repository. This is in preparation for being able to select polar cap data by pole, type, and apparition rather than loading all polar cap data.

--- MarsPolarAnalysis001.py
+++ MarsPolarAnalysis001.py
@@ -15,9 +15,10 @@ import MarsPolarLibrary as MPL
 
 ###############################################################################
 #Mars Polar Observations as a function of season
-fn="f:\\Astronomy\Projects\Planets\Mars\Imaging Data\Mapping\HillMars_mea.CSV"
+fn="f:/Astronomy/Projects/Planets/Mars/Imaging Data/Mapping/HillMars_mea.CSV"
 
 MarsObs=MPL.MarsMeasData(fn)
+MarsObs.load_all_data()
 All_Ls_array=np.array(MarsObs.hlon)-(35.4377+49.5581)
     
 canvas=pl.figure(figsize=(6.5, 4.5), dpi=150,facecolor="white")
@@ -28,7 +29,7 @@ for Pole in ["North","South"]:
     Target=Pole+" Pole"
     canvas.add_subplot(3,1,1+temp,axisbg="white") 
     temp=temp+2   
-    y0,y1,ytks=30.,90.,13
+    y0,y1,ytks=25.,90.,14
     
     pl.xlim(x0,x1)
     pl.xticks(np.linspace(x0,x1,xtks, endpoint=True))
@@ -48,6 +49,7 @@ for Pole in ["North","South"]:
     for CaporHood in ["Cap","Hood"]:
     
         Indices = [k for k, x in enumerate(MarsObs.PoleandObject) if x[1:4] == Pole[0]+"P"+CaporHood[0]] #what does this do!?
+        #THIS IS WHERE TO INSERT A LOOP FOR APPARITION FOR COLOR CYCLING
         Latitude=[MarsObs.Latitude[index] for index in Indices]
         hlon=[MarsObs.hlon[index] for index in Indices]
         Ls_array=np.array(hlon)-(35.4377+49.5581)
@@ -63,13 +65,21 @@ for Pole in ["North","South"]:
         LS_ALPO=np.linspace(40,135,20)
         LAT_ALPO=[65.1,67.4,67.6,68.0,69.7,72.2,74.0,75.5,77.8,79.8,
                   81.2,81.6,82.7,83.1,82.9,82.4,82.8,83.5,83.6,83.4]
+        LAT_SD_ALPO=[4.4,4.5,3.4,2.4,2.4,2.1,1.4,1.8,2.8,2.9,
+                     2.5,2.2,1.3,1.5,1.6,1.8,1.9,1.0,0.7,0.6]
         pl.plot(LS_ALPO,LAT_ALPO,color='k',label="ALPO 1980-2003 Average")
+        pl.plot(LS_ALPO,np.array(LAT_ALPO)+1.96*np.array(LAT_SD_ALPO),color='k',linewidth=0.5,label="95% Confidence")
+        pl.plot(LS_ALPO,np.array(LAT_ALPO)-1.96*np.array(LAT_SD_ALPO),color='k',linewidth=0.5)
     elif Pole=="South":
         LS_ALPO=np.linspace(200,300,21)-360.
         W_ALPO=[54.0,51.2,49.1,47.4,44.4,40.4,38.2,35.1,31.3,28.9,23.9,
                 21.6,20.6,19.2,16.8,16.0,14.6,12.1,9.8,9.5,10.6]
+        LAT_SD_ALPO=[7.1,6.5,4.6,5.5,4.9,7.5,7.8,6.5,6.1,5.4,4.2,
+                     3.1,1.7,2.2,2.2,2.3,3.0,3.2,1.8,1.4,1.0]
         LAT_ALPO=-(90.-np.array(W_ALPO)/2.)
         pl.plot(LS_ALPO,LAT_ALPO,color='k',label="ALPO 1986-2003 Average")
+        pl.plot(LS_ALPO,np.array(LAT_ALPO)+1.96*np.array(LAT_SD_ALPO),color='k',linewidth=0.5,label="95% Confidence")
+        pl.plot(LS_ALPO,np.array(LAT_ALPO)-1.96*np.array(LAT_SD_ALPO),color='k',linewidth=0.5)
         
     pl.legend(loc=4,ncol=1,fontsize=6,scatterpoints=1)
 ###############################################################################
@@ -123,5 +133,9 @@ pl.xlabel("Solar Longitude, Ls (deg)",fontsize=7)
 pl.title("LS Coverage",fontsize=9)
 
 pl.scatter(All_Ls_array,date,marker='o',s=10,color='k',alpha=1.0,linewidths=0)
+DateIndices = [k for k, x in enumerate(date) if x > np.datetime64("2018-01-01")] #what does this do!?
+for i in DateIndices:
+    print date[i]
 
+print date>np.datetime64("2018-01-01")
 pl.savefig("f:\\Astronomy\Projects\Planets\Mars\Imaging Data\Mapping\Mars_Coverage.png",dpi=300)
--- MarsPolarLibrary.py
+++ MarsPolarLibrary.py
@@ -5,32 +5,32 @@ Created on Fri Jul 29 07:42:34 2016
 @author: Astronomy
 """
 
-class MarsMeasData:
-    def __init__(self,filename):
+
+import sys
+drive='f:'
+sys.path.append(drive+'\\Astronomy\Python Play\Util')
+
+import ConfigFiles as CF
+
+class MarsMeasData(CF.readtextfilelines):
+    pass
+    def load_all_data(self):
         import ephem
         import numpy as np
        
-        #        
         self.DateTimeString=[]
         self.PoleandObject=[]
         self.Latitude=[]
         self.hlon=[]
         self.hlat=[]
-        self.NRecords=0
 
-        MarsMeasData_DataFile=open(filename,'r')
-        MarsMeasData_Lines=MarsMeasData_DataFile.readlines()
-        MarsMeasData_DataFile.close()
-        nrecords=len(MarsMeasData_Lines)
-        self.NRecords=nrecords
-
-        for i in range(1,nrecords):
-            fields=MarsMeasData_Lines[i].split(',')
-            #print fields
-
+        for recordindex in range(1,self.nrecords):
+            fields=self.CfgLines[recordindex].split(',')
+            print fields[5][7:8]
             tempdate=fields[4][1:11]
             temptime1=fields[5][1:6]
             temptime2=fields[5][7:8]
+            print temptime2
             if int(temptime2) <=1:
                 datetimestring= tempdate+" "+temptime1+":0"+str(6*int(temptime2))#+"UT"
             elif int(temptime2) >1:
