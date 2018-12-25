12664413355779b365f6d7de4b23cca007a28148
Steven Hill <smhill001@gmail.com>
2018-12-25 15:28:05 -0700

    Working Version with Colors
    A number of changes have been made so that now polar cap and hood data can be selected by apparition, plotted with different colors and the error bars of the ALPO data for polar cap retreat are now plotted. Also, cosmetic improvements have been made in the labeling etc.

--- MarsPolarAnalysis001.py
+++ MarsPolarAnalysis001.py
@@ -16,102 +16,52 @@ import MarsPolarLibrary as MPL
 ###############################################################################
 #Mars Polar Observations as a function of season
 fn="f:/Astronomy/Projects/Planets/Mars/Imaging Data/Mapping/HillMars_mea.CSV"
-
-MarsObs=MPL.MarsMeasData(fn)
-MarsObs.load_all_data()
-All_Ls_array=np.array(MarsObs.hlon)-(35.4377+49.5581)
-    
-canvas=pl.figure(figsize=(6.5, 4.5), dpi=150,facecolor="white")
-x0,x1,xtks=-180.,180.,13
 temp=0
 
-for Pole in ["North","South"]:
-    Target=Pole+" Pole"
-    canvas.add_subplot(3,1,1+temp,axisbg="white") 
-    temp=temp+2   
-    y0,y1,ytks=25.,90.,14
-    
-    pl.xlim(x0,x1)
-    pl.xticks(np.linspace(x0,x1,xtks, endpoint=True))
-    
-    if Pole=="North":
-        pl.ylim(y0,y1)
-        pl.yticks(np.linspace(y0,y1,ytks, endpoint=True))
-    elif Pole=="South":
-        pl.ylim(-y1,-y0)
-        pl.yticks(np.linspace(-y1,-y0,ytks, endpoint=True))
-        
-    pl.grid()
-    pl.tick_params(axis='both', which='major', labelsize=6)
-    pl.ylabel("Latitude (deg)",fontsize=7)
-    pl.title(Target,fontsize=9)
+OppositionDates=["2005-11-07","2007-12-24","2010-01-29","2012-03-03",
+                 "2014-04-08","2016-05-22","2018-07-27"]
+LineColorList=np.array([[0.4,0.2,0.6],[0.1,0.1,0.7],[0.1,0.6,0.1],
+                        [0.6,0.5,0.2],[0.6,0.1,0.0],[0.0,0.0,0.0],
+                        [0.5,0.5,0.5]])
 
+axN,axS=MPL.SetupMarsPlot()
+
+MarsObs=MPL.MarsMeasData(fn)
+for od in OppositionDates:
+    MarsObs.load_select_data("Test","Test",od)
+    
     for CaporHood in ["Cap","Hood"]:
     
-        Indices = [k for k, x in enumerate(MarsObs.PoleandObject) if x[1:4] == Pole[0]+"P"+CaporHood[0]] #what does this do!?
-        #THIS IS WHERE TO INSERT A LOOP FOR APPARITION FOR COLOR CYCLING
+        Indices = [k for k, x in enumerate(MarsObs.PoleandObject) if x[1:4] == "NP"+CaporHood[0]] #what does this do!?
         Latitude=[MarsObs.Latitude[index] for index in Indices]
         hlon=[MarsObs.hlon[index] for index in Indices]
         Ls_array=np.array(hlon)-(35.4377+49.5581)
         Ls_array=np.mod(Ls_array+180.0,360.)-180.
         if CaporHood=="Cap":
-            clr='k'
+            mkr='o'
+            axN.scatter(np.array(Ls_array,dtype=float),np.array(Latitude,dtype=float),
+                       marker=mkr,s=15,color=LineColorList[np.mod(temp,7)],alpha=1.0,linewidths=0,label=od)       
         elif CaporHood=="Hood":
-            clr='b'
-        pl.scatter(np.array(Ls_array,dtype=float),np.array(Latitude,dtype=float),
-                   marker='o',s=10,color=clr,alpha=1.0,linewidths=0,label=CaporHood)
-    
-    if Pole=="North":
-        LS_ALPO=np.linspace(40,135,20)
-        LAT_ALPO=[65.1,67.4,67.6,68.0,69.7,72.2,74.0,75.5,77.8,79.8,
-                  81.2,81.6,82.7,83.1,82.9,82.4,82.8,83.5,83.6,83.4]
-        LAT_SD_ALPO=[4.4,4.5,3.4,2.4,2.4,2.1,1.4,1.8,2.8,2.9,
-                     2.5,2.2,1.3,1.5,1.6,1.8,1.9,1.0,0.7,0.6]
-        pl.plot(LS_ALPO,LAT_ALPO,color='k',label="ALPO 1980-2003 Average")
-        pl.plot(LS_ALPO,np.array(LAT_ALPO)+1.96*np.array(LAT_SD_ALPO),color='k',linewidth=0.5,label="95% Confidence")
-        pl.plot(LS_ALPO,np.array(LAT_ALPO)-1.96*np.array(LAT_SD_ALPO),color='k',linewidth=0.5)
-    elif Pole=="South":
-        LS_ALPO=np.linspace(200,300,21)-360.
-        W_ALPO=[54.0,51.2,49.1,47.4,44.4,40.4,38.2,35.1,31.3,28.9,23.9,
-                21.6,20.6,19.2,16.8,16.0,14.6,12.1,9.8,9.5,10.6]
-        LAT_SD_ALPO=[7.1,6.5,4.6,5.5,4.9,7.5,7.8,6.5,6.1,5.4,4.2,
-                     3.1,1.7,2.2,2.2,2.3,3.0,3.2,1.8,1.4,1.0]
-        LAT_ALPO=-(90.-np.array(W_ALPO)/2.)
-        pl.plot(LS_ALPO,LAT_ALPO,color='k',label="ALPO 1986-2003 Average")
-        pl.plot(LS_ALPO,np.array(LAT_ALPO)+1.96*np.array(LAT_SD_ALPO),color='k',linewidth=0.5,label="95% Confidence")
-        pl.plot(LS_ALPO,np.array(LAT_ALPO)-1.96*np.array(LAT_SD_ALPO),color='k',linewidth=0.5)
+            mkr='+'
+            axN.scatter(np.array(Ls_array,dtype=float),np.array(Latitude,dtype=float),
+                       marker=mkr,s=15,color=LineColorList[np.mod(temp,7)],alpha=1.0,linewidths=0)
         
-    pl.legend(loc=4,ncol=1,fontsize=6,scatterpoints=1)
+        Indices = [k for k, x in enumerate(MarsObs.PoleandObject) if x[1:4] == "SP"+CaporHood[0]] #what does this do!?
+        Latitude=[MarsObs.Latitude[index] for index in Indices]
+        hlon=[MarsObs.hlon[index] for index in Indices]
+        Ls_array=np.array(hlon)-(35.4377+49.5581)
+        Ls_array=np.mod(Ls_array+180.0,360.)-180.
+        if CaporHood=="Cap":
+            mkr='o'
+        elif CaporHood=="Hood":
+            mkr='+'
+        axS.scatter(np.array(Ls_array,dtype=float),np.array(Latitude,dtype=float),
+                   marker=mkr,s=15,color=LineColorList[np.mod(temp,7)],alpha=1.0,linewidths=0,label=od)
+    temp=temp+1
+    axN.legend(loc=2,ncol=3,fontsize=6,scatterpoints=1)
+    #axS.legend(loc=2,ncol=3,fontsize=6,scatterpoints=1)
 ###############################################################################
 #Solar Latitude as a function of season
-Target="Solar Latitude"
-
-canvas.add_subplot(3,1,2,axisbg="white") 
-y0,y1,ytks=-30.,30.,13
-pl.xlim(x0,x1)
-# Set x ticks
-pl.xticks(np.linspace(x0,x1,xtks, endpoint=True))
-# Set y limits
-pl.ylim(y0,y1)
-# Set y ticks
-pl.yticks(np.linspace(y0,y1,ytks, endpoint=True))
-pl.grid()
-pl.tick_params(axis='both', which='major', labelsize=6)
-pl.ylabel("Solar Latitude (deg)",fontsize=7)
-
-pl.title(Target,fontsize=9)
-
-LS_array=np.linspace(-180.,180.,73.)
-SolLat=25.*np.sin(LS_array*np.pi/180.)
-
-pl.plot(LS_array,SolLat,color='k',label="Solar Latitude (deg)")
-
-Seasons=['Autumn','Winter','Spring','Summer']
-for j in range(0,len(Seasons)):
-    pl.annotate(Seasons[j],xy=(-135.+90.*j,27.5),size=7,horizontalalignment='center',verticalalignment='center')
-    pl.annotate(Seasons[np.mod(j+2,4)],xy=(-135.+90.*j,-27.5),size=7,horizontalalignment='center',verticalalignment='center')
-    
-pl.subplots_adjust(left=0.1, right=0.9, top=0.95, bottom=0.09)
 
 pl.savefig("f:\\Astronomy\Projects\Planets\Mars\Imaging Data\Mapping\Mars_Seasons.png",dpi=300)
 
@@ -119,11 +69,15 @@ pl.savefig("f:\\Astronomy\Projects\Planets\Mars\Imaging Data\Mapping\Mars_Season
 #Mars Seasonal Coverage by Terrestrial Year
 canvas=pl.figure(figsize=(4.5, 4.5), dpi=150,facecolor="white")
 canvas.add_subplot(1,1,1,axisbg="white") 
+
+MarsObs.load_all_data()
+All_Ls_array=np.array(MarsObs.hlon)-(35.4377+49.5581)
 All_Ls_array=np.mod(All_Ls_array+180.0,360.)-180.
 date=[]
 for i in range(0,len(MarsObs.DateTimeString)):
     date.append(np.datetime64(MarsObs.DateTimeString[i]))
 
+x0,x1,xtks=-180.,180.,13
 pl.xlim(x0,x1)
 pl.xticks(np.linspace(x0,x1,xtks, endpoint=True))
 pl.grid()
@@ -133,6 +87,7 @@ pl.xlabel("Solar Longitude, Ls (deg)",fontsize=7)
 pl.title("LS Coverage",fontsize=9)
 
 pl.scatter(All_Ls_array,date,marker='o',s=10,color='k',alpha=1.0,linewidths=0)
+
 DateIndices = [k for k, x in enumerate(date) if x > np.datetime64("2018-01-01")] #what does this do!?
 for i in DateIndices:
     print date[i]
--- MarsPolarLibrary.py
+++ MarsPolarLibrary.py
@@ -17,8 +17,10 @@ class MarsMeasData(CF.readtextfilelines):
     def load_all_data(self):
         import ephem
         import numpy as np
+        import MarsPolarLibrary as MPL
        
         self.DateTimeString=[]
+        self.DateTime=[]
         self.PoleandObject=[]
         self.Latitude=[]
         self.hlon=[]
@@ -26,18 +28,9 @@ class MarsMeasData(CF.readtextfilelines):
 
         for recordindex in range(1,self.nrecords):
             fields=self.CfgLines[recordindex].split(',')
-            print fields[5][7:8]
-            tempdate=fields[4][1:11]
-            temptime1=fields[5][1:6]
-            temptime2=fields[5][7:8]
-            print temptime2
-            if int(temptime2) <=1:
-                datetimestring= tempdate+" "+temptime1+":0"+str(6*int(temptime2))#+"UT"
-            elif int(temptime2) >1:
-                datetimestring= tempdate+" "+temptime1+":"+str(6*int(temptime2))#+"UT"
-            datetimestring=datetimestring.replace("/", "-")
-
+            datetimestring=MPL.MakeDateTime(fields[4],fields[5])
             self.DateTimeString.append(datetimestring)
+            self.DateTime.append(np.datetime64(datetimestring))
             self.PoleandObject.append(fields[1])
             self.Latitude.append(float(fields[10]))
             
@@ -46,3 +39,137 @@ class MarsMeasData(CF.readtextfilelines):
             self.hlon.append(hltemp)
             hltemp=float(Planet.hlat)*180./np.pi
             self.hlat.append(hltemp)
+
+    def load_select_data(self,Pole,Type,OppositionDate):
+        import ephem
+        import numpy as np
+        import MarsPolarLibrary as MPL
+       
+        self.DateTimeString=[]
+        self.DateTime=[]
+        self.PoleandObject=[]
+        self.Latitude=[]
+        self.hlon=[]
+        self.hlat=[]
+
+        Odate64=np.datetime64(OppositionDate)
+        for recordindex in range(1,self.nrecords):
+            fields=self.CfgLines[recordindex].split(',')
+            datetimestring=MPL.MakeDateTime(fields[4],fields[5])
+            dt64=np.datetime64(datetimestring)
+            if Odate64-180<=dt64<=Odate64+180:
+                self.DateTimeString.append(datetimestring)
+                self.DateTime.append(np.datetime64(datetimestring))
+                self.PoleandObject.append(fields[1])
+                self.Latitude.append(float(fields[10]))
+            
+                Planet = ephem.Mars(datetimestring)
+                hltemp=float(Planet.hlon)*180./np.pi
+                self.hlon.append(hltemp)
+                hltemp=float(Planet.hlat)*180./np.pi
+                self.hlat.append(hltemp)
+            
+def MakeDateTime(datefield,timefield):
+    tempdate=datefield[1:11]
+    temptime1=timefield[1:6]
+    temptime2=timefield[7:8]
+    #print temptime2
+    if int(temptime2) <=1:
+        datetimestring= tempdate+" "+temptime1+":0"+str(6*int(temptime2))#+"UT"
+    elif int(temptime2) >1:
+        datetimestring= tempdate+" "+temptime1+":"+str(6*int(temptime2))#+"UT"
+    datetimestring=datetimestring.replace("/", "-")
+    return datetimestring
+
+def SetupMarsPlot():
+    import matplotlib.pyplot as pl
+    import numpy as np
+ 
+    canvas=pl.figure(figsize=(8.0, 6.0), dpi=150,facecolor="white")
+    x0,x1,xtks=-180.,180.,13
+    y0,y1,ytks=25.,90.,14
+
+    #NORTH
+    axN=canvas.add_subplot(3,1,1,axisbg="white") 
+    
+    axN.set_xlim(x0,x1)
+    axN.set_xticks(np.linspace(x0,x1,xtks, endpoint=True))
+    
+    axN.set_ylim(y0,y1)
+    axN.set_yticks(np.linspace(y0,y1,ytks, endpoint=True))
+        
+    axN.grid(linewidth=0.5)
+    axN.tick_params(axis='y', which='major', labelsize=9)
+    axN.tick_params(labelbottom=False)
+    axN.set_ylabel("Latitude (deg)",fontsize=9)
+    axN.set_title("North Pole",fontsize=10)
+    
+    LS_ALPO=np.linspace(40,135,20)
+    LAT_ALPO=[65.1,67.4,67.6,68.0,69.7,72.2,74.0,75.5,77.8,79.8,
+              81.2,81.6,82.7,83.1,82.9,82.4,82.8,83.5,83.6,83.4]
+    LAT_SD_ALPO=[4.4,4.5,3.4,2.4,2.4,2.1,1.4,1.8,2.8,2.9,
+                 2.5,2.2,1.3,1.5,1.6,1.8,1.9,1.0,0.7,0.6]
+    axN.plot(LS_ALPO,LAT_ALPO,color='k',label="ALPO 1980-2003 Average")
+    axN.plot(LS_ALPO,np.array(LAT_ALPO)+1.96*np.array(LAT_SD_ALPO),color='k',linewidth=0.5,label="95% Confidence")
+    axN.plot(LS_ALPO,np.array(LAT_ALPO)-1.96*np.array(LAT_SD_ALPO),color='k',linewidth=0.5)
+    
+    #SOUTH
+    axS=canvas.add_subplot(3,1,3,axisbg="white") 
+    y0,y1,ytks=25.,90.,14
+    
+    axS.set_xlim(x0,x1)
+    axS.set_xticks(np.linspace(x0,x1,xtks, endpoint=True))
+    axS.set_ylim(-y1,-y0)
+    axS.set_yticks(np.linspace(-y1,-y0,ytks, endpoint=True))
+       
+    axS.grid(linewidth=0.5)
+    axS.tick_params(axis='both', which='major', labelsize=8)
+    axS.set_ylabel("Latitude (deg)",fontsize=9)
+    axS.set_xlabel("Solar Longitude (deg)",fontsize=9)
+    axS.set_title("South Pole",fontsize=10)
+    
+    LS_ALPO=np.linspace(200,300,21)-360.
+    W_ALPO=[54.0,51.2,49.1,47.4,44.4,40.4,38.2,35.1,31.3,28.9,23.9,
+            21.6,20.6,19.2,16.8,16.0,14.6,12.1,9.8,9.5,10.6]
+    LAT_SD_ALPO=[7.1,6.5,4.6,5.5,4.9,7.5,7.8,6.5,6.1,5.4,4.2,
+                 3.1,1.7,2.2,2.2,2.3,3.0,3.2,1.8,1.4,1.0]
+    LAT_ALPO=-(90.-np.array(W_ALPO)/2.)
+    axS.plot(LS_ALPO,LAT_ALPO,color='k',label="ALPO 1986-2003 Average")
+    axS.plot(LS_ALPO,np.array(LAT_ALPO)+1.96*np.array(LAT_SD_ALPO),color='k',linewidth=0.5,label="95% Confidence")
+    axS.plot(LS_ALPO,np.array(LAT_ALPO)-1.96*np.array(LAT_SD_ALPO),color='k',linewidth=0.5)
+    
+    #SOLAR LATITUDE
+    Target="Solar Latitude"
+
+    canvas.add_subplot(3,1,2,axisbg="white") 
+    y0,y1,ytks=-30.,30.,13
+    pl.xlim(x0,x1)
+    # Set x ticks
+    pl.xticks(np.linspace(x0,x1,xtks, endpoint=True))
+    # Set y limits
+    pl.ylim(y0,y1)
+    # Set y ticks
+    pl.yticks(np.linspace(y0,y1,ytks, endpoint=True))
+    pl.grid(linewidth=0.5)
+    pl.tick_params(axis='y', which='major', labelsize=9)
+    pl.tick_params(labelbottom=False)
+    pl.ylabel("Solar Latitude (deg)",fontsize=9)
+    
+    pl.title(Target,fontsize=9)
+    
+    LS_array=np.linspace(-180.,180.,73.)
+    SolLat=25.*np.sin(LS_array*np.pi/180.)
+    
+    pl.plot(LS_array,SolLat,color='k',label="Solar Latitude (deg)")
+    
+    Seasons=['Autumn','Winter','Spring','Summer']
+    for j in range(0,len(Seasons)):
+        pl.annotate(Seasons[j],xy=(-135.+90.*j,27.5),size=9,horizontalalignment='center',verticalalignment='center')
+        pl.annotate(Seasons[np.mod(j+2,4)],xy=(-135.+90.*j,-27.5),size=9,horizontalalignment='center',verticalalignment='center')
+        
+    pl.subplots_adjust(left=0.1, right=0.9, top=0.95, bottom=0.09)
+
+    return axN,axS
+
+
+   
